f571b349f813fb2f16fe538b7a6442f05b2375fe
Josh Peak <neozenith.dev@gmail.com>
2019-01-16 15:27:52 +1100

    refactored server connection setup and multipart MIME

--- send_email.py
+++ send_email.py
@@ -1,6 +1,36 @@
 import base64
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import getpass
+from string import Template
+
+server = {"host": "localhost", "port": 1025, "login": "neozenith.dev@gmail.com"}
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "multipart test"
+message["From"] = "neozenith.dev@gmail.com"
+message["To"] = "neozenith.dev@gmail.com"
+text = """\
+Hi,
+How are you?
+Real Python has many great tutorials:
+www.realpython.com"""
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       <a href="http://www.realpython.com">Real Python</a>
+       has many great tutorials.
+       $(chart)
+    </p>
+  </body>
+</html>
+"""
+
+message.attach(MIMEText(text, "plain"))
+message.attach(MIMEText(html, "html"))
 
 
 def embed_html_b64image(filename, alt_text="", encoding="image/png", style=""):
@@ -13,41 +43,31 @@ def embed_html_b64image(filename, alt_text="", encoding="image/png", style=""):
     return f"<img alt='{alt_text}' style='{style}'src='data:{encoding};base64,{b64fileContent.decode()}'/>"
 
 
-def main():
-    print("Welcome")
-
-    sender_email = "neozenith.dev@gmail.com"
-    smtp_server = "localhost"
-    port = 1025
-
-    receiver_email = "neozenith.dev@gmail.com"
-    message = """\
-Subject: Hi there
-
-    This message is sent from Python."""
-
+def connect_email_server(host, port, user="", password=None):
     # Create a secure SSL context
     context = ssl.create_default_context()
 
-    # Try to log in to server and send email
-    try:
-        server = smtplib.SMTP(smtp_server, port)
-        server.ehlo()  # Can be omitted
-        if smtp_server != "localhost":
-            server.starttls(context=context)  # Secure the connection
-            server.ehlo()  # Can be omitted
+    email_server = smtplib.SMTP(host, port)
+    email_server.ehlo()  # Can be omitted
+    if server["host"] != "localhost":
+        email_server.starttls(context=context)  # Secure the connection
+        email_server.ehlo()  # Can be omitted
 
-            password = getpass.getpass(prompt="Type your password and press enter: ")
-            server.login(sender_email, password)
+        password = getpass.getpass(prompt="Type your password and press enter: ")
+        email_server.login(user, password)
 
-        server.sendmail(sender_email, receiver_email, message)
+    return email_server
 
-        # TODO: Send email here
-    except Exception as e:
-        # Print any error messages to stdout
-        print(e)
-    else:
-        server.quit()
+
+def main():
+    print("Welcome")
+
+    email_server = connect_email_server(server["host"], server["port"], server["login"])
+
+    if email_server:
+        email_server.sendmail(message["From"], message["To"], message.as_string())
+
+        email_server.quit()
 
 
 if __name__ == "__main__":
