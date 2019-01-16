a86e96f43d09c03cbc1170ba5da9287c8f15deca
Josh Peak <neozenith.dev@gmail.com>
2019-01-16 13:48:15 +1100

    A version with TLS that should work

--- send_email.py
+++ send_email.py
@@ -16,17 +16,27 @@ def embed_html_b64image(filename, alt_text="", encoding="image/png", style=""):
 def main():
     print("Welcome")
 
-    login_user = "neozenith.dev@gmail.com"
-
-    smtp_host = "smtp.gmail.com"
-    port = 465  # For SSL
+    sender_email = "neozenith.dev@gmail.com"
+    smtp_server = "smtp.gmail.com"
+    port = 587
     password = getpass.getpass(prompt="Type your password and press enter: ")
 
     # Create a secure SSL context
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL(smtp_host, port, context=context) as server:
-        server.login(login_user, password)
+    # Try to log in to server and send email
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)  # Secure the connection
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        # TODO: Send email here
+    except Exception as e:
+        # Print any error messages to stdout
+        print(e)
+    else:
+        server.quit()
 
 
 if __name__ == "__main__":
